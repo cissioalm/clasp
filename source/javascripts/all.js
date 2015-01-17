4ea61115ac8e5c334a3ec9579656aff40b79664c
Will <whp3652@rit.edu>
2015-01-16 21:01:50 -0500

    Fix the mobile collapse

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -1,1 +1,15 @@
 //= require_tree .
+
+$(document).ready(function() {
+  var menuToggle = $('#js-banner-mobile-menu').unbind();
+  $('#js-banner-menu').removeClass("show");
+
+  menuToggle.on('click', function(e) {
+    e.preventDefault();
+    $('#js-banner-menu').slideToggle(function(){
+      if($('#js-banner-menu').is(':hidden')) {
+        $('#js-banner-menu').removeAttr('style');
+      }
+    });
+  });
+});
