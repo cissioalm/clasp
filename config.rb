700e0823c8a982590cb7402586a56c3ec09f2683
Will <whp3652@rit.edu>
2015-01-16 22:59:23 -0500

    Add cards to the people page

--- config.rb
+++ config.rb
@@ -43,7 +43,7 @@ helpers do
 end
 
 # Automatic image dimensions on image_tag helper
-activate :automatic_image_sizes
+# activate :automatic_image_sizes
 
 # Reload the browser automatically whenever files change
 configure :development do
