418410b89a3310e1bc32ceeded187dc86dbc0adf
Will <whp3652@rit.edu>
2015-01-25 13:45:55 -0500

    Fix news links to home page

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -1,6 +1,7 @@
 //= require_tree .
 
 $(document).ready(function() {
+  // Mobile banner toggling
   var menuToggle = $('#js-banner-mobile-menu').unbind();
   $('#js-banner-menu').removeClass("show");
 
@@ -13,15 +14,38 @@ $(document).ready(function() {
     });
   });
 
+  // Load recent news to the front page
   if ($.contains(document.body, $("#js-news")[0])) {
     $.get("news.html", function(data) {
-      var vdom = $('<div></div>');
+      var vdom = $('<section></section>'),
+          $dom_entry = $("#js-news"),
+          max_articles = 5,
+          num_articles,
+          $articles;
+
       vdom.html(data);
-      console.log($(".news > article", vdom)[0]);
-      $("#js-news").append($(".news > article", vdom)[0]);
+      $articles = $(".news > article", vdom);
+      num_articles = $articles.length;
+      // console.log(num_articles);
+
+      $articles.each(function(i) {
+        if (i < max_articles) {
+          var $date = $(".date", this),
+              $title = $(".title", this),
+              $output = $('<article class="news-articles"></article>');
+
+          $title.wrap("<a href='news.html' class='js-news-links'></a>");
+          $title = $(".js-news-links", this);
+          $output.append($date).append($title);
+          $dom_entry.append($output);
+        }
+      });
+
+      // .append($(".news > article", vdom)[0]);
     });
   }
 
+  // Enable table functionality on the publications page
   if ($.contains(document.body, $("#js-publications")[0])) {
     $(function () { $("#js-publications").footable({
         addRowToggle: false
